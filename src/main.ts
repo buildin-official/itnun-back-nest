32bd656a38624ec2d018f99cc611728905541a9b
seonwoo0808 <seonwoo0808@pentag.kr>
2023-05-01 22:32:45 +0900

    Add logger and log file writer

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { setting } from './setting';
+import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(setting.was.port);
 }
