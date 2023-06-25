5cea60f47b36d253334aeb4b29196184d0d867eb
Tilto_ <tilto0822@gmail.com>
2023-06-25 09:36:20 +0900

    add recruit api detail info query function

--- src/api/recruitInfoAPI.service.ts
+++ src/api/recruitInfoAPI.service.ts
@@ -63,12 +63,11 @@ export class RecruitInfoAPIService {
       pfPreferential?: Array<'05' | '07' | '08' | '09' | '10' | '14' | 'S' | 'B'>;
       workHrCd?: Array<'1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '99'>;
     },
-    callType: 'L' | 'D' = 'L',
   ) {
     const response = await this._apiClient.get(
       `/opi/opi/opia/wantedApi.do?${paramMaker({
         authKey,
-        callTp: callType,
+        callTp: 'L',
         returnType: 'xml',
         startPage: page,
         display: count,
@@ -117,5 +116,102 @@ export class RecruitInfoAPIService {
     }
 
     return result;
+  }
+
+  public async queryDetail(authKey: string, wantedAuthNo: string) {
+    const response = await this._apiClient.get(
+      `/opi/opi/opia/wantedApi.do?${paramMaker({
+        authKey,
+        wantedAuthNo,
+        callTp: 'L',
+        returnType: 'xml',
+        infoSvc: 'VALIDATION',
+      })}`,
+    );
+
+    console.log(response);
+
+    const $ = load((await response).data, {
+      xml: true,
+    });
+
+    const wantedData = $('wantedDtl');
+    const corpInfo = wantedData.find('corpInfo');
+    const wantedInfo = wantedData.find('wantedInfo');
+    const empchargeInfo = wantedData.find('empchargeInfo');
+
+    return {
+      wantedAuthNo: wantedData.find('wantedAuthNo').text(),
+      corpInfo: {
+        corpNm: corpInfo.find('corpNm').text(),
+        reperNm: corpInfo.find('reperNm').text(),
+        totPsncnt: Number(corpInfo.find('totPsncnt').text()),
+        capitalAmt: Number(corpInfo.find('capitalAmt').text()),
+        yrSalesAmt: Number(corpInfo.find('yrSalesAmt').text()),
+        indTpCdNm: corpInfo.find('indTpCdNm').text(),
+        busiCont: corpInfo.find('busiCont').text(),
+        corpAddr: corpInfo.find('corpAddr').text(),
+        homePg: corpInfo.find('homePg').text(),
+        busiSize: corpInfo.find('busiSize').text(),
+      },
+      wantedInfo: {
+        jobsNm: wantedInfo.find('jobsNm').text(),
+        wantedTitle: wantedInfo.find('wantedTitle').text(),
+        relJobsNm: wantedInfo.find('relJobsNm').text(),
+        jobCont: wantedInfo.find('jobCont').text(),
+        receiptCloseDt: wantedInfo.find('receiptCloseDt').text(),
+        empTpNm: wantedInfo.find('empTpNm').text(),
+        collectPsncnt: wantedInfo.find('collectPsncnt').text(),
+        salTpNm: wantedInfo.find('salTpNm').text(),
+        enterTpNm: wantedInfo.find('enterTpNm').text(),
+        eduNm: wantedInfo.find('eduNm').text(),
+        forLang: wantedInfo.find('forLang').text(),
+        major: wantedInfo.find('major').text(),
+        certificate: wantedInfo.find('certificate').text(),
+        mltsvcExcHope: wantedInfo.find('mltsvcExcHope').text(),
+        compAbl: wantedInfo.find('compAbl').text(),
+        pfCond: wantedInfo.find('pfCond').text(),
+        etcPfCond: wantedInfo.find('etcPfCond').text(),
+        selMthd: wantedInfo.find('selMthd').text(),
+        rcptMthd: wantedInfo.find('rcptMthd').text(),
+        submitDoc: wantedInfo.find('submitDoc').text(),
+        etcHopeCont: wantedInfo.find('etcHopeCont').text(),
+        workRegion: wantedInfo.find('workRegion').text(),
+        nearLine: wantedInfo.find('nearLine').text(),
+        workdayWorkhrCont: wantedInfo.find('workdayWorkhrCont').text(),
+        fourIns: wantedInfo.find('fourIns').text(),
+        retirepay: wantedInfo.find('retirepay').text(),
+        etcWelfare: wantedInfo.find('etcWelfare').text(),
+        disableCvntl: wantedInfo.find('disableCvntl').text(),
+        //! 아래 3개 뭔가 배열로 줘야 할 것 같은데 테스트해보고 알려주세요
+        attachFileInfo: {
+          attachFileUrl: wantedInfo.find('attachFileInfo').find('attachFileUrl').text(),
+        },
+        corpAttachList: {
+          attachFileUrl: wantedInfo.find('corpAttachList').find('attachFileUrl').text(),
+        },
+        keywordList: {
+          srchKeywordNm: wantedInfo.find('keywordList').find('srchKeywordNm').text(),
+        },
+        dtlRecrContUrl: wantedInfo.find('dtlRecrContUrl').text(),
+        jobsCd: wantedInfo.find('jobsCd').text(),
+        minEdubgIcd: wantedInfo.find('minEdubgIcd').text(),
+        maxEdubgIcd: wantedInfo.find('maxEdubgIcd').text(),
+        regionCd: wantedInfo.find('regionCd').text(),
+        empTpCd: wantedInfo.find('empTpCd').text(),
+        enterTpCd: wantedInfo.find('enterTpCd').text(),
+        salTpCd: wantedInfo.find('salTpCd').text(),
+        staAreaRegionCd: wantedInfo.find('staAreaRegionCd').text(),
+        lineCd: wantedInfo.find('lineCd').text(),
+        staNmCd: wantedInfo.find('staNmCd').text(),
+        exitNoCd: wantedInfo.find('exitNoCd').text(),
+        walkDistCd: wantedInfo.find('walkDistCd').text(),
+      },
+      empchargeInfo: {
+        empChargerDpt: empchargeInfo.find('empChargerDpt').text(),
+        contactTelno: empchargeInfo.find('contactTelno').text(),
+        chargerFaxNo: empchargeInfo.find('chargerFaxNo').text(),
+      },
+    };
   }
 }
