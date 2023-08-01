eed3182b4263d2949749f055aa8f3ed65399f14e
seonwoo0808 <seonwoo0808@pentag.kr>
2023-08-01 19:36:17 +0900

    test change

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "itnun-back-nest"', () => {
+      expect(appController.getHello()).toBe('itnun-back-nest');
     });
   });
 });
